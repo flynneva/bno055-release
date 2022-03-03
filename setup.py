e027c98c1a02c8a903d0eae1f8ea332b7be0945e
Evan Flynn <evanflynn.msu@gmail.com>
2022-03-02 21:58:27 -0800

    Imported upstream version '0.3.0' of 'upstream'

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages
 from setuptools import setup
 
@@ -5,7 +7,7 @@ package_name = 'bno055'
 
 setup(
     name=package_name,
-    version='0.2.0',
+    version='0.3.0',
     # find sub-packages automatically in order to allow sub-modules, etc. to be imported:
     # packages=[package_name],
     packages=find_packages(exclude=['test']),
@@ -16,6 +18,8 @@ setup(
         ('share/ament_index/resource_index/packages',
          ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('bno055/params/*.yaml'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
